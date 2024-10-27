19d4bc893eef3fb6669c1d1f3e38a95dd8af80c6
levmn <levimncontato@gmail.com>
2024-10-27 01:36:01 -0300

    Adicionando README

---
+++ README.md
@@ -0,0 +1,14 @@
+# AutoCheck 🚗🔧
+
+**AutoCheck** foi desenvolvida em parceria com a **Porto Seguro** para otimizar a gestão de agendamentos e diagnósticos automotivos. A aplicação permite que usuários façam agendamentos de serviços e acompanhem diagnósticos de veículos de maneira eficiente.
+
+## Equipe
+
+- **Allan Brito Moreira** - RM558948
+- **Caio Liang** - RM558868
+- **Levi Magni** - RM98276
+
+## Estrutura do Projeto
+
+- **jdbc/**: Contém o arquivo `.jar` do **ojdbc** para conexão com o banco de dados Oracle e o script SQL para criação das tabelas.
+- **.env.sample**: Arquivo de configuração que deve ser duplicado e renomeado para `.env`, onde serão inseridas as credenciais do banco de dados.
--- src/main/java/br/com/autocheck/app/Main.java
+++ src/main/java/br/com/autocheck/app/Main.java
@@ -1,22 +1,37 @@
 package br.com.autocheck.app;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import br.com.autocheck.connection.Conexao;
+
+/*
+ * Testando conexão com o banco de dados
+ * */
 
 public class Main {
 	public static void main(String[] args) {
 
-		Conexao conexao = null;
+		Conexao conexao = new Conexao();
+		Connection conn = null;
 
 		try {
-			conexao = new Conexao();
-			System.out.println("Conexão estabelecida com sucesso!");
+			conn = conexao.conexao();
+			if (conn != null) {
+				System.out.println("Conexão estabelecida com sucesso!");
+			} else {
+				System.out.println("Falha ao estabelecer a conexão.");
+			}
 		} finally {
-			if (conexao != null) {
-				System.out.println();
-				conexao.closeConnection();
+			if (conn != null) {
+				System.out.println("Encerrando conexão...");
+				try {
+					conn.close();
+					System.out.println("Conexão encerrada com sucesso.");
+				} catch (SQLException e) {
+					System.err.println("Erro ao encerrar a conexão: " + e.getMessage());
+				}
 			}
 		}
-
 	}
-
 }
